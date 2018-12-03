b49eb075dd0aa4655268d08045eb420cf408d3fd
chenchen <chenchen@finupgroup.com>
2018-12-03 19:37:57 +0800

    添加商品跳转新页面

--- src/components/page/Updategoods.vue
+++ src/components/page/Updategoods.vue
@@ -13,7 +13,6 @@
             <el-button @click="handleSearch" slot="append" icon="el-icon-search"></el-button>
         </el-input>
 
-
         <el-table
             :data="goodsList"
             style="width: 100%"
@@ -117,55 +116,8 @@
             background
             layout="prev, pager, next"
             @current-change="currentChange"
-            :page-count="6">
+            :page-count="pageCount">
         </el-pagination>
-
-        <el-dialog title="添加商品" :visible.sync="dialogFormVisible">
-            <el-form ref="form" :model="form" label-width="80px">
-                <el-form-item label="活动名称">
-                    <el-input v-model="form.name"></el-input>
-                </el-form-item>
-                <el-form-item label="活动区域">
-                    <el-select v-model="form.region" placeholder="请选择活动区域">
-                        <el-option label="区域一" value="shanghai"></el-option>
-                        <el-option label="区域二" value="beijing"></el-option>
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="活动时间">
-                    <el-col :span="11">
-                        <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-                    </el-col>
-                    <el-col class="line" :span="2">-</el-col>
-                    <el-col :span="11">
-                        <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-                    </el-col>
-                </el-form-item>
-                <el-form-item label="即时配送">
-                    <el-switch v-model="form.delivery"></el-switch>
-                </el-form-item>
-                <el-form-item label="活动性质">
-                    <el-checkbox-group v-model="form.type">
-                        <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                        <el-checkbox label="地推活动" name="type"></el-checkbox>
-                        <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                        <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-                    </el-checkbox-group>
-                </el-form-item>
-                <el-form-item label="特殊资源">
-                    <el-radio-group v-model="form.resource">
-                        <el-radio label="线上品牌商赞助"></el-radio>
-                        <el-radio label="线下场地免费"></el-radio>
-                    </el-radio-group>
-                </el-form-item>
-                <el-form-item label="活动形式">
-                    <el-input type="textarea" v-model="form.desc"></el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click="onSubmit">立即创建</el-button>
-                    <el-button>取消</el-button>
-                </el-form-item>
-            </el-form>
-        </el-dialog>
     </div>
 </template>
 
@@ -189,7 +141,13 @@
                 },
                 search: '',
                 select: '',
-                dialogFormVisible: false
+                totalCount: undefined,
+                singleItems: 10
+            }
+        },
+        computed: {
+            pageCount(){
+                return Math.ceil(this.totalCount/this.singleItems);
             }
         },
         methods:{
@@ -213,7 +171,7 @@
             //编辑
             handleEdit(index,row){
                 console.log(index,row);
-                this.dialogFormVisible = true;
+                this.$router.replace('/addgoods')
             },
 
             //删除
@@ -232,6 +190,7 @@
                     let {code,data} = await axios.get(`${GET_GOODS_LIST}?${param}`);
                     if(code === 2000){
                         this.goodsList = data.data;
+                        this.totalCount = data.totalCount;
                     }
                 }
             },
@@ -242,7 +201,7 @@
         },
 
         created(){
-            this.getGoodsList({pageIndex:1,pageSize:10});
+            this.getGoodsList({pageIndex:1,pageSize:this.singleItems});
         }
     }
 </script>
